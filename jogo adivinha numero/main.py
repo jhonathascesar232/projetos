a87b6d55c807723420df692e4edda479ebc6cc84
jhonathascesar232 <51928763+jhonathascesar232@users.noreply.github.com>
2022-07-08 17:52:25 -0300

    Revert "set up do jogo 21"

---
+++ jogo adivinha numero/main.py
@@ -0,0 +1,28 @@
+import random
+import math
+
+# obten as entradas do usuário
+menor = int(input('** Digite o valor MENOR do intervalo: '))
+maior = int(input('** Digite o valor MAIOR intervalo: '))
+
+# gera um número aleatório
+x = random.randint(menor, maior)
+
+# calcula as chances com base nos intervalos
+chances = round(math.log(maior - menor + 1, 2))
+print(f'Você tem {chances}!')
+
+cont = 0
+while cont < chances:  # loop do jogo
+    cont += 1
+    entrada_usuario = int(input('** Numero:\n** '))
+    if entrada_usuario == x:
+        print('** Parabéns, GANHOU **')
+        break
+    elif x > entrada_usuario:
+        print('** Seu palpite foi muito baixo')
+    elif x < entrada_usuario:
+        print('** Seu palpite foi muito alto')
+if cont > chances:
+    print('** O número é: {x}')
+    print('** Melhor na proxima')
